614ef2a48fbe25dc1e147dba56034053f5ceac79
seeyh111 <34484702+seeyh111@users.noreply.github.com>
2019-08-18 18:18:05 +0800

    Add files via upload

---
+++ farm2/environment.py
@@ -0,0 +1,87 @@
+import sys
+import Adafruit_DHT
+from time import sleep
+from rpi_lcd import LCD
+from gpiozero import MCP3008, Buzzer
+import RPi.GPIO as GPIO 
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import json
+
+bz = Buzzer(21)
+pin = 4
+lcd = LCD()
+update = True
+mcp3008 = MCP3008(channel=0)
+
+host = "a3f69y8dukmi8a-ats.iot.us-east-1.amazonaws.com"
+rootCAPath = "rootca.pem"
+certificatePath = "certificate.pem.crt"
+privateKeyPath = "private.pem.key"
+
+my_rpi = AWSIoTMQTTClient("farm2-environment")
+my_rpi.configureEndpoint(host, 8883)
+my_rpi.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+my_rpi.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+my_rpi.configureDrainingFrequency(2)  # Draining: 2 Hz
+my_rpi.configureConnectDisconnectTimeout(10)  # 10 sec
+my_rpi.configureMQTTOperationTimeout(5)  # 5 sec
+
+def buzzer():
+    if temperature < 0 or humidity > 100:
+        #if too cold or too humid
+        bz.on()
+        sleep(2)
+        bz.toggle()
+    else:
+        bz.off()
+        sleep(1)
+
+def publisher():
+    message = {}
+    message["deviceid"] = "farm2"
+    import datetime as datetime
+    now = datetime.datetime.now()
+    message["datetime_value"] = now.strftime("%d/%m/%Y, %H:%M:%S")
+    message["temperature_value"] = temperature
+    message["humidity_value"] = humidity
+    message["light_value"] = light
+    print("Publishing environment values to farm database")
+    my_rpi.publish("sensors/environment/farm", json.dumps(message), 1)
+
+def bzCallback(client, userdata, message):
+        print('reach bzCallback')
+        data = json.loads(message.payload)
+        if data['status'] == 'On':
+            bz.on()
+            sleep(2)
+        elif data['status'] == 'Off':
+            bz.off()
+
+my_rpi.connect()
+my_rpi.subscribe("buzz/status/farm2", 1, bzCallback)
+
+try:
+    while update:
+        try:
+            humidity, temperature = Adafruit_DHT.read_retry(11, pin)
+            light = (1024*(1.0-mcp3008.value))
+            light = round(light)
+            lcd.text('Temp: {:.1f} C'.format(temperature), 1)
+            lcd.text('Humidity: {:.1f}'.format(humidity), 2)
+            print('Temp: {:.1f} C'.format(temperature))
+            print('Humidity: {:.1f}'.format(humidity))
+            buzzer()
+            publisher()
+            print("Wait a few seconds before getting next temperature and humidity values..")
+            sleep(2)
+
+        except KeyboardInterrupt:
+            update=False
+            lcd.clear()
+        except:
+            print("Error while inserting data...")
+            print(sys.exc_info()[0])
+            print(sys.exc_info()[1])
+except:
+    print(sys.exc_info()[0])
+    print(sys.exc_info()[1])
---
+++ farm2/soil.py
@@ -0,0 +1,95 @@
+#device libraries
+import sys
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+from gpiozero import Button
+from time import sleep 
+import datetime as datetime
+import json
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+from rpi_lcd import LCD
+lcd = LCD()
+
+#Soil setup
+soil = Button(24)
+
+#amazonaws host and certs
+host = "a3f69y8dukmi8a-ats.iot.us-east-1.amazonaws.com"
+rootCAPath = "rootca.pem"
+certificatePath = "certificate.pem.crt"
+privateKeyPath = "private.pem.key"
+
+#mqtt connection
+my_rpi = AWSIoTMQTTClient("farm2-irrigation")
+my_rpi.configureEndpoint(host, 8883)
+my_rpi.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+my_rpi.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+my_rpi.configureDrainingFrequency(2)  # Draining: 2 Hz
+my_rpi.configureConnectDisconnectTimeout(10)  # 10 sec
+my_rpi.configureMQTTOperationTimeout(5)  # 5 sec
+
+def checking():
+    try:
+        publisher()
+        lcd.text('Soil is dry',1)
+        lcd.text('SNS alert sent',2)
+        print("Soil moisture is dry... ")
+        print("Sending SNS alert...") 
+        sleep(1)
+        print("SNS alert sent")
+        sleep(2)
+        #Check moisture reading again
+        lcd.text('Checking soil',1)
+        lcd.text('Moisture',2)
+        print("Checking soil moisture again..")
+    except Exception as e: 
+        print(e)
+        print('error')
+        print(sys.exc_info()[0])
+        print(sys.exc_info()[1])
+
+def publisher():
+    try:
+        import datetime as datetime
+        import json
+        message = {}
+        message["deviceid"] = "farm2"
+        now = datetime.datetime.now()
+        message["datetime_value"] = now.strftime("%d/%m/%Y, %H:%M:%S")
+        message["alert"] = 1		
+        my_rpi.publish("farm/pump", json.dumps(message), 1)
+    except Exception as e: 
+        print(e)
+        print('error')
+        print(sys.exc_info()[0])
+        print(sys.exc_info()[1])
+            
+my_rpi.connect()
+print("Successfully connected to MQTT!")
+
+update = True
+while update:
+    try:
+        lcd = LCD()
+        if soil.is_held:
+            lcd.text('Soil is wet',1)
+            print("Soil moisture is wet")
+            sleep(10)
+
+        else:
+            print("Checking Soil moisture after 10 seconds...")
+            sleep(5)
+            checking()
+            sleep(3)
+
+    except Exception as e: 
+        print(e)
+        print('error')
+        print(sys.exc_info()[0])
+        print(sys.exc_info()[1])
+                      
+    except KeyboardInterrupt:
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(23,GPIO.IN)
+		GPIO.cleanup()
+                update = False
